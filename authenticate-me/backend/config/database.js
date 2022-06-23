eab4ae2ddccf9512ccd1459a9b1f70f0b0c178cd
Christian Alcantara <chriscayana@gmail.com>
2022-06-23 14:54:00 -0700

    fixed

--- authenticate-me/backend/config/database.js
+++ authenticate-me/backend/config/database.js
@@ -15,15 +15,9 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize'
   },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+  "production": {
+    "dialect": "postgres",
+    "seederStorage": "sequelize",
+    "use_env_variable": "DATABASE_URL"
   }
 };
