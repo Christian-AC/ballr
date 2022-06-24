a1b49c74ce841a9027d079abf97da59c77bb49d9
Christian Alcantara <chriscayana@gmail.com>
2022-06-24 15:01:30 -0700

    created users/pictures/albums db

---
+++ backend/db/migrations/20220624212153-create-image.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      albumId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING(250)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Images');
+  }
+};
