aec05fd9ce9b2a42f9da52340771f5ec44a0b114
Christian Alcantara <chriscayana@gmail.com>
2022-06-27 09:31:04 -0700

    finshing getAllImages thunk

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -6,17 +6,27 @@ const router = express.Router();
 router.get(
   '/',
   asyncHandler(async function(req, res) {
-      const images = await Image.findAll();
-      return res.json(images);
+    const images = await Image.findAll();
+    return res.json(images);
   })
 );
 
+router.get(
+  '/user/:id',
+  asyncHandler(async function(req, res) {
+    const userId = req.params.id;
+    const images = await Image.findAll({
+      where: { userId: userId}
+    });
+    return res.json(images);
+  })
+);
 
 router.get(
   '/:id',
   asyncHandler(async function(req, res) {
-      const images = await Image.findByPk(req.params.id);
-      return res.json(images);
+    const images = await Image.findByPk(req.params.id);
+    return res.json(images);
   })
 );
 
